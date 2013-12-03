6d0eadef65630e9aebf13c9912b50d9ff0988680
Mikko Korpela <mikko.korpela@gmail.com>
2013-12-03 14:59:35 +0200

    overrides

--- decorator_exercises.py
+++ decorator_exercises.py
@@ -27,3 +27,25 @@ def test_xml_tag():
         stream.write('moi')
     eq_(stream.getvalue(), '<foo>moi</foo>')
 
+
+# Overrides
+def overrides(interface_class):
+    def overrider(method):
+        assert(method.__name__ in dir(interface_class))
+        return method
+    return overrider
+
+class SuperDuberClass(object):
+
+    def method(self):
+        return 3
+
+@raises(AssertionError)
+def test_overrides_usage():
+
+    class Subclass(SuperDuberClass):
+        
+        def methud(self):
+            return 5
+
+
