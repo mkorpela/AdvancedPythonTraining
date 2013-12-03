8249853d551bc8a72306a32ab796ed2e87d970f0
Mikko Korpela <mikko.korpela@gmail.com>
2013-12-03 14:53:01 +0200

    decorator property and contextmanager

--- decorator_exercises.py
+++ decorator_exercises.py
@@ -1,1 +1,29 @@
 # Decorator related exercises
+from nose.tools import eq_, raises
+
+# Property
+class ClassWithProperty(object):
+
+    def __init__(self, value_for_property):
+        self.value = value_for_property
+
+@raises(AttributeError)
+def test_property():
+    obj = ClassWithProperty(7)
+    eq_(obj.value, 7)
+    obj.value = 8
+
+
+# Context manager
+from contextlib import contextmanager
+from StringIO import StringIO
+
+def tag(tag, stream):
+    raise NotImplementedError('Implement this')
+
+def test_xml_tag():
+    stream = StringIO()
+    with tag('foo', stream):
+        stream.write('moi')
+    eq_(stream.getvalue(), '<foo>moi</foo>')
+
