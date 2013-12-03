c7b6acce8c1b9c4ae8a12170fe0af6e3b8437b1e
Mikko Korpela <mikko.korpela@gmail.com>
2013-12-03 15:04:26 +0200

    making simple decorator

--- decorator_exercises.py
+++ decorator_exercises.py
@@ -1,5 +1,5 @@
 # Decorator related exercises
-from nose.tools import eq_, raises
+from nose.tools import eq_, raises, timed
 
 # Property
 class ClassWithProperty(object):
@@ -49,3 +49,61 @@ def test_overrides_usage():
             return 5
 
 
+# Making a simple decorator
+
+def add_one_decorator(func):
+    raise NotImplementedError('Implement this')
+
+def test_simple_decorator():
+
+    @add_one_decorator
+    def return_four():
+        return 4
+
+    eq_(return_four(), 5)
+
+def cache_result_decorator(func):
+    """ Implement this! """
+    return func
+
+import timeit
+
+@timed(1.0)
+def test_fibonacci_performance():
+    @cache_result_decorator
+    def fib(n):
+        if n <= 1:
+            return 1
+        return fib(n-1)+fib(n-2)
+    fib(35)
+    
+# Decorator with an argument
+
+def add_some_decorator(some_number):
+    raise NotImplementedError('Implement this')
+
+def test_decorator_with_argument():
+    @add_some_decorator(3)
+    def one():
+        return 1
+    @add_some_decorator(5)
+    def two():
+        return 2
+    eq_(one(), 4)
+    eq_(two(), 7)
+
+# Decorator as a class
+
+class MyDecoratorClass(object):
+
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args):
+        raise NotImplementedError('Implement this')
+
+def test_decorator_class():
+    @MyDecoratorClass
+    def say_hello(name):
+        return 'hello %s' % name
+    eq_(say_hello('steve'), 'HELLO STEVE')
