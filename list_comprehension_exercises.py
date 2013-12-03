fca16e1be3e1a45cb9a6212d1caf424ca966f48b
Mikko Korpela <mikko.korpela@gmail.com>
2013-12-03 17:33:49 +0200

    list comprehension

--- list_comprehension_exercises.py
+++ list_comprehension_exercises.py
@@ -1,1 +1,19 @@
 # List comprehension
+from nose.tools import eq_
+
+# LIST COMPREHENSION
+# [ RESULT(i) for i in iterable if CONDITION(i) ]
+
+def make_this_work_with_list_comprehension(list_of_numbers):
+    result = []
+    for number in list_of_numbers:
+        if number % 2 == 1:
+            result.append((number+5) % 7)
+    return result
+
+def test_that_it_still_works():
+    f = make_this_work_with_list_comprehension
+    eq_(f([]), [])
+    eq_(f([1,2,3,4,5]), [6,1,3])
+    eq_(f([90, 91, 92, 93, 94, 95, 96, 97, 98, 99]), [5, 0, 2, 4, 6])
+
