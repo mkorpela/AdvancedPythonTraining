1d53ead431598a72f337c358b5dccaa1f745045b
Mikko Korpela <mikko.korpela@gmail.com>
2013-12-04 08:00:38 +0200

    generator

--- generator_exercises.py
+++ generator_exercises.py
@@ -1,4 +1,4 @@
-from nose.tools import eq_
+from nose.tools import eq_, ok_
 
 def fibs():
     raise NotImplementedError('Implement this')
@@ -13,4 +13,21 @@ def test_fibonacci_generator():
         eq_(f, prev+prev2)
         prev, prev2 = f, prev
 
+def primes():
+    raise NotImplementedError('Implement this')
+
+def test_primes_generator():
+    prims = primes()
+    eq_(prims.next(), 2)
+    eq_(prims.next(), 3)
+    eq_(prims.next(), 5)
+    p = prims.next()
+    eq_(prims.next(), p)
+    while p < 1000:
+        p, previous = prims.next(), p
+        ok_(p > previous)
+        for p2 in primes():
+            if p2**2 > p:
+                break
+            ok_(p % p2 != 0)
 
