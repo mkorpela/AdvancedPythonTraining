428fca62fc4edc86ab19a3a7471a54f42a9cea90
Mikko Korpela <mikko.korpela@gmail.com>
2013-12-04 08:10:39 +0200

    xml

--- xml_exercises.py
+++ xml_exercises.py
@@ -1,1 +1,35 @@
 # XML exercises
+from nose.tools import ok_, eq_
+
+MY_SMALL_XML = """
+<books>
+    <book type="fiction">
+        <name>Iltalehti</name>
+    </book>
+    <book type="facts">
+        <name>aku ankka</name>
+    </book>
+    <book type="documentation">
+        <name>Robot Framework User Guide</name>
+    </book>
+</books>
+"""
+
+def get_names(xml_string):
+    raise NotImplementedError('Implement this')
+
+def test_parsing_book_names():
+    names = get_names(MY_SMALL_XML)
+    ok_('aku ankka' in names)
+    ok_('Iltalehti' in names)
+    ok_('Robot Framework User Guide' in names)
+
+def get_type_by_name(name, xml_string):
+    raise NotImplementedError('Implement this')
+
+def test_getting_type_by_name():
+    eq_('facts', get_type_by_name('aku ankka', MY_SMALL_XML))
+    eq_('documentation', get_type_by_name('Robot Framework User Guide', MY_SMALL_XML))
+
+# SHOW ROBOT FRAMEWORK REPORTING AND HOW XML IS HANDLED THERE!!!!!!
+
